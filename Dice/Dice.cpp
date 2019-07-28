4fdbc174eaaf8f72bd90e7c0c499c692c47365ac
string.Empty <mystring.empty@gmail.com>
2019-07-28 14:09:36 +0800

    修复讨论组黑名单检测无法退群

--- Dice/Dice.cpp
+++ Dice/Dice.cpp
@@ -660,6 +660,7 @@ EVE_DiscussMsg_EX(eventDiscussMsg)
 		AddMsgToQueue(strMsg, eve.fromDiscuss, Discuss);
 		Sleep(1000);
 		sendAdmin(printChat({ eve.fromDiscuss,Discuss }) + strMsg);
+		setDiscussLeave(eve.fromDiscuss);
 	}
 	FromMsg Msg(eve.message, eve.fromDiscuss, Discuss, eve.fromQQ);
 	if (Msg.DiceFilter())eve.message_block();
