9622641e6e199345f24dae7e1613e57a708c27c0
w4123 <1840686745@qq.com>
2020-06-25 23:26:15 +0800

    发布 2.4.0beta3

--- Dice/GlobalVar.h
+++ Dice/GlobalVar.h
@@ -35,9 +35,9 @@
  * 请勿修改Dice_Build, Dice_Ver_Without_Build，DiceRequestHeader以及Dice_Ver常量
  * 请修改Dice_Short_Ver或Dice_Full_Ver常量以达到版本自定义
  */
-const unsigned short Dice_Build = 562u;
-inline const std::string Dice_Ver_Without_Build = "2.4.0beta2";
-constexpr auto DiceRequestHeader = "Dice/2.4.0BETA2";
+const unsigned short Dice_Build = 563u;
+inline const std::string Dice_Ver_Without_Build = "2.4.0beta3";
+constexpr auto DiceRequestHeader = "Dice/2.4.0BETA3";
 inline const std::string Dice_Ver = Dice_Ver_Without_Build + "(" + std::to_string(Dice_Build) + ")";
 inline const std::string Dice_Short_Ver = "Dice! by 溯洄 Shiki Ver " + Dice_Ver;
 
